54a39dbdb25bbf16a43e2a6fd42ada4106184644
Lloyd Kayembe <kayembeloyd@gmail.com>
2021-01-09 16:48:38 +0200

    Fixing 'controller not found issue'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\OrganizationsController;
+use App\Http\Controllers\JobsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
